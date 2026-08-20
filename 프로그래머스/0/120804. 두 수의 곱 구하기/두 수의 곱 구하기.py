51f9d784457c6e5b5a08b1d72a8e4d850e00e5d6
galaxyko2026-maker <galaxyko2026@gmail.com>
2026-08-20 18:42:04 +0900

    [level 0] Title: 두 수의 곱 구하기, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.py
@@ -0,0 +1,15 @@
+def solution(num1, num2):
+    answer = 0
+    return answer
+
+num1 = 3
+num2 = 4
+def solution(num1, num2):
+    answer = num1*num2
+    return answer
+
+num1 = 27
+num2 = 19
+def solution(num1, num2):
+    answer = num1*num2
+    return answer
